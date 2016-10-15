c2c98c4f3a95ecbba168abb81cc1df4e50af405a
jnrkufuor <jaycuffs@gmail.com>
2016-10-15 21:14:55 +0000

    new

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 (function() {
 
-	"use strict";
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 	var pictureSource;
 	var destinationType;
@@ -11,18 +10,9 @@
 		document.getElementById("capturePhoto").onclick = function() {
 			navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
 				quality : 50,
+
 				destinationType : destinationType.DATA_URL
 			});
-		};
-
-		function onPhotoDataSuccess(imageData) {
-			var smallImage = document.getElementById('smallImage');
-			smallImage.style.display = 'block';
-			smallImage.src = "data:image/jpeg;base64," + imageData;
-		}
-
-		function onFail(message) {
-			alert('Failed because: ' + message);
 		}
 
 		document.getElementById("barcode").onclick=function barcode(){
@@ -33,5 +23,25 @@
 			}
 												 );
 		};
+
+	};
+	function onPhotoDataSuccess(imageData) {
+
+		var smallImage = document.getElementById('smallImage');
+
+		smallImage.style.display = 'block';
+
+		smallImage.src = "data:image/jpeg;base64," + imageData;
+
 	}
+
+	function onFail(message) {
+
+		alert('Failed because: ' + message);
+
+	}
+
+
 })();
+
+
