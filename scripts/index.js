f3606a37c2d4d7c46eb4afeefa21f5eb24f121c9
jnrkufuor <jaycuffs@gmail.com>
2016-10-20 10:46:19 +0000

    new

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,33 @@
-(function() {
+(function () {
+  'use strict';
+  document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+  var pictureSource, destinationType;
+  function onDeviceReady() {
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType;
 
-	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-	var pictureSource;
-	var destinationType;
-	function onDeviceReady() {
-		pictureSource = navigator.camera.PictureSourceType;
-		destinationType = navigator.camera.DestinationType;
+    document.getElementById("capturePhoto").onclick = function () {
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+        quality : 50,
+        destinationType : destinationType.DATA_URL
+      });
+    };
 
-		document.getElementById("capturePhoto").onclick = function() {
-			navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-				quality : 50,
+    document.getElementById("geolocationdata").addEventListener("click", function () {
+      navigator.geolocation.getCurrentPosition(onSuccess, Error, {enableHighAccuracy: true});
+    });
 
-				destinationType : destinationType.DATA_URL
-			});
-		}
+    var watchId = navigator.geolocation.watchPosition(Success, Error);
+    
+    document.getElementById("geowatch").addEventListener("click", function () {
+      navigator.geolocation.watchPosition(watchId);
+    });
 
-		/*document.getElementById("barcode").onclick=function barcode(){
+
+
+
+
+    /*document.getElementById("barcode").onclick=function barcode(){
 			cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
 				alert("encode success: " + success);
 			}, function(fail) {
@@ -24,22 +36,43 @@
 												 );
 		};*/
 
-	};
-	function onPhotoDataSuccess(imageData) {
+  }
 
-		var smallImage = document.getElementById('smallImage');
 
-		smallImage.style.display = 'block';
+  function onPhotoDataSuccess(imageData) {
 
-		smallImage.src = "data:image/jpeg;base64," + imageData;
+    var smallImage = document.getElementById('smallImage');
 
-	}
+    smallImage.style.display = 'block';
 
-	function onFail(message) {
+    smallImage.src = "data:image/jpeg;base64," + imageData;
 
-		alert('Failed because: ' + message);
+  }
 
-	}
+  function onFail(message) {
+
+    alert('Failed because: ' + message);
+
+  }
+
+  function Success(position)
+  {
+    document.getElementById("watchSpace").innerHTML = 'Latitude: ' + position.coords.latitude + '<br>' +
+      'Longitude ' + position.coords.longitude + '<br>' +
+      '<hr/>' +  document.getElementById("watchSpace").innerHTML;
+  }
+
+
+  function Error(error) {
+    alert('code' + error.code + '\n' +
+          'message' + error.message + '\n');
+  }
+
+
+  var onSuccess = function (position) {
+    alert("Latitude:" + position.coords.latitude + '\n' +
+          "Longitude" + position.coords.longitude + '\n');
+  };
 
 
 })();
